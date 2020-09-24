7d0cd2e41bba4b3664fe2508a723872761887c12
silas.00 <silasdasilva.00@gmail.com>
2020-09-24 12:37:12 -0300

    Aplicativo TOP

---
+++ src/br/com/psg/cadastro/view/App.java
@@ -0,0 +1,26 @@
+package br.com.psg.cadastro.view;
+
+import java.net.URL;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+	
+	public static void main(String[] args) {
+		launch();
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		URL fxml = getClass().getResource("./cadastro.fxml");
+		Parent parent = FXMLLoader.load(fxml);
+		stage.setTitle("Cadastro de pessoa!");
+		stage.setScene(new Scene(parent));
+		stage.show();
+	}
+
+}
